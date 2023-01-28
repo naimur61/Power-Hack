17cd4f42cff6cce08a2ee52cdcb7cdf4586e882a
Naimur <naimur.r61n@gmail.com>
2023-01-29 01:20:31 +0600

    first commit

---
+++ src/Routes/Routes/Routes.js
@@ -0,0 +1,19 @@
+import { createBrowserRouter } from 'react-router-dom';
+import Main from '../../LayOut/Main';
+
+
+
+export const Routes = createBrowserRouter([
+
+   // Main Layout 
+   {
+      path: '/', element: <Main />, children: [
+         // { path: '/', element: <Login /> },
+         // { path: '/home', element: <Home /> },
+         // { path: '/details/:id', element: <PrivetRoutes><Details /></PrivetRoutes>, loader: ({ params }) => fetch(`https://m-server-pi.vercel.app/posts/${params.id}`) },
+
+      ]
+   },
+
+])
+
